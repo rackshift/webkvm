d7fe6f24e687346aea3a216a0ec72ab1f6501789
zhangdahai112 <dahai.zhang@fit2cloud.com>
2021-07-21 17:47:24 +0800

    refactor:整合

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM registry.cn-qingdao.aliyuncs.com/x-lab/kvm-base:v1.0.0 
 
-RUN apt-get install -y nodejs && \
-cp -rf scripts/* /
+RUN apt-get install -y nodejs
+
+COPY ./scripts /
 
 COPY startup.sh /startapp.sh
--- startup.sh
+++ startup.sh
@@ -9,17 +9,17 @@ if [ -z $VENDOR ];then
   echo "Inspur,DELL,H3C,Supermicro,Suma"
   exit 1
 fi
-
+dir=/scripts
 if [[ $VENDOR == DEL* ]];then
-  dir=/dell
+  dir=$dir/dell
 elif [[ $VENDOR == Inspur* ]];then
-  dir=/inspur
+  dir=$dir/inspur
 elif [[ $VENDOR == *H3C* ]];then
-  dir=/h3c
+  dir=$dir/h3c
 elif [[ $VENDOR == Supermicro* ]];then
-  dir=/supermicro
+  dir=$dir/supermicro
 elif [[ $VENDOR == Suma* ]];then
-  dir=/sugo
+  dir=$dir/sugo
 else
   echo "Not supported Vendor: $VENDOR"
   exit 1
